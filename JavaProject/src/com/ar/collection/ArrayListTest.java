6fd9cf424c4996f0475a92373d119fcf37904f58
Arun Gupta <shivam.arun2012@gmail.com>
2018-09-16 00:55:51 +0530

    ArrayList Java program

--- JavaProject/src/com/ar/collection/ArrayListTest.java
+++ JavaProject/src/com/ar/collection/ArrayListTest.java
@@ -1,6 +1,46 @@
+//ArrayList with String objects
 package com.ar.collection;
 
-public class ArrayListTest {
+import java.util.ArrayList;
+import java.util.Iterator;
 
+public class ArrayListTest {
+    public static void main (String[] args) {
+
+
+        //create ArrayList
+        ArrayList<String> al = new ArrayList<String>();
+        // add 5 objects
+        al.add("Apple");
+        al.add("Orange");
+        al.add("Mango");
+        al.add("Grapes");
+        al.add("Guava");
+        al.add("Apple");
+
+        //display contents
+        System.out.println("Contents:" +al);
+
+        //remove objects
+        al.remove(3);
+        al.remove("Apple");
+
+        //display again
+        System.out.println("Contents after removing:"+al);
+
+        //display size
+        System.out.println("Size of ArrayList:"+al.size());
+
+        //extract elements using Iterator
+        System.out.println("Extracting using Iterator :");
+
+        //add an Iterator to ArrayList to retrieve elements
+        Iterator it = al.iterator();
+
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
+
+    }
 
 }
